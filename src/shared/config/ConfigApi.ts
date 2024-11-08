389ada58177f0c071c084e504f7ee1af2768eda8
BMBSOFT\hien.pham <hien.pham@bmbsoft.com.vn>
2024-11-08 10:37:27 +0700

    update

--- src/shared/config/ConfigApi.ts
+++ src/shared/config/ConfigApi.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://192.168.3.70:3000';
-export const DOC_URL = `http://192.168.3.70:3000/api/static-contents/`;
+export const BASE_URL = 'http://192.168.3.69:3000';
+export const DOC_URL = `http://192.168.3.69:3000/api/static-contents/`;
